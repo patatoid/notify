5956924aabb23604c2603b8152c799a14000ce23
Pascal Knoth <pascal@knoth.fr>
2015-08-14 20:15:50 +0200

    notifications class method for model

--- lib/active_record_extensions/notify.rb
+++ lib/active_record_extensions/notify.rb
@@ -2,7 +2,7 @@ module Notify
 	extend ActiveSupport::Concern
 	
 	def notify!(method, result)
-		Notify::Notification.create(user: self.class.current_user, klass: self.class, action: method, result: result)
+		Notify::Notification.create(user: self.class.current_user, klass: self.class.name, action: method, result: result)
 		logger.debug "#{self.class.current_user} : #{self.class} -> #{method} #=> #{result.inspect}"
 	end
 
@@ -11,9 +11,14 @@ module Notify
 			methods.each do |method|
 				alias_method "#{method}_without_notification".to_sym, method
 				define_method(method) do |*args|
-					notify!(method, send("#{method}_without_notification", *args))
+					result = send("#{method}_without_notification", *args)
+					notify!(method, result)
+					result
 				end
 			end
+		end
+		def notifications
+			Notify::Notification.where(klass: name)
 		end
 	end
 end
